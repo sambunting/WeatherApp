105f6dabcc8d6b61105565dcc17dd68ac0b1a735
sambunting <sambunting2008@live.co.uk>
2019-06-29 16:23:14 +0100

    Made the mobile keyboard hide on enter

--- frontend/src/components/search/search.js
+++ frontend/src/components/search/search.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 import './search.css';
 
-
-
 class Search extends React.Component {
     textUpdate = (e) => {
         if (e.keyCode === 13) {
             this.props.onSearch(e.target.value);
+            this.refs.input.disabled = true;
+
+            setTimeout(this.refs.input.disabled = false, 100);
         }
     }
 
     render() {
         return (
             <div className="search">
-            <input type="text" id="search_input" name="search_input" placeholder="Search" onKeyDown={this.textUpdate}></input>
+            <input ref="input" type="text" id="search_input" name="search_input" placeholder="Search" onKeyDown={this.textUpdate}></input>
             <img src="icons/Compass-small.svg" onClick={this.props.onGeolocationRequest} alt="location"/>
             </div>
         )
