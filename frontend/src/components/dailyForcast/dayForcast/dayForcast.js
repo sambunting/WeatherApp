cdee7376005d6ec3ab2cda0058f53c26e53382c1
sambunting <sambunting2008@live.co.uk>
2019-06-29 15:57:25 +0100

    Added temprature to dayForcast

--- frontend/src/components/dailyForcast/dayForcast/dayForcast.js
+++ frontend/src/components/dailyForcast/dayForcast/dayForcast.js
@@ -4,7 +4,7 @@ import './dayForcast.css'
 
 class DayForcast extends React.Component {
     render() {
-        const { icon, summary, time } = this.props.data;
+        const { icon, summary, time, temperatureHigh } = this.props.data;
 
         const date = new Date(time * 1000);
         const day = new Intl.DateTimeFormat('en-GB', { weekday: 'long'}).format(date);
@@ -12,10 +12,13 @@ class DayForcast extends React.Component {
 
         return (
             <div className="dayForcast">
-                <img className="title__icon" src={`icons/${icon}.svg`} alt={icon}/>
+                <img className="dayForcast__icon" src={`icons/${icon}.svg`} alt={icon}/>
                 <div className="dayForcast__left">
                     <p className="dayForcast__left-date">{dateString}</p>
                     <p className="dayForcast__left-summary">{summary}</p>
+                </div>
+                <div className="dayForcast__right">
+                    <h3>{Math.round(temperatureHigh)}<span>°C</span></h3>
                 </div>
             </div>
         )
